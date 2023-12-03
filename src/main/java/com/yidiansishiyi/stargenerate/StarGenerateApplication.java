9137e18cd3e5470118de4798a57fff55cb4b1c6d
threeSeven <nuomibaicha@163.com>
2023-12-03 11:03:30 +0800

    init 代码初始化仓库

---
+++ src/main/java/com/yidiansishiyi/stargenerate/StarGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.yidiansishiyi.stargenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarGenerateApplication.class, args);
+    }
+
+}
